24fa09f5176d2bdf83778eabaa3ffe69305140ff
dhruvpendharkar <66141152+dhruvpendharkar@users.noreply.github.com>
2026-01-09 09:35:42 -0600

    Fixed import issue in init file to use correct Qwen3 trainer

--- src/trainer/__init__.py
+++ src/trainer/__init__.py
@@ -1,3 +1,3 @@
-from .grpo_trainer import Qwen2VLGRPOTrainer
+from .grpo_trainer import Qwen3VLGRPOTrainer
 from .grpo_config import GRPOConfig
-__all__ = ["Qwen2VLGRPOTrainer"]
+__all__ = ["Qwen3VLGRPOTrainer"]
